08c8a9269bcd8f634e18c8faa7b1426bee710c29
Zelalem Belay <zelalem.belay1@gmail.com>
2025-08-28 22:41:23 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -87,7 +87,7 @@ export default function App() {
 
     setSubmissionStatus('sending'); // Set status to indicate sending
 
-    const apiUrl = 'https://bf52bca7012e.ngrok-free.api/mailApi';
+    const apiUrl = 'https://bf52bca7012e.ngrok-free.app/mailApi';
     const formData = {
       name: name,
       email: email,
